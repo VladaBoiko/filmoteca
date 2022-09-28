c837025b7b63347b2c35c6085324d8c3d1f00d1f
VladaBoiko <bermezha@gmail.com>
2022-09-28 21:33:09 +0300

    fvafv

---
+++ src/js/render.js
@@ -0,0 +1,44 @@
+import { dataMovieList } from './API/api';
+import { dataGenre } from './API/api';
+const galleryListEl = document.querySelector('#gallery-list');
+async function getAllGenres(lang = 'en-US') {
+  return await dataGenre((language = lang));
+}
+async function request() {
+  const data = await dataMovieList((page = 1), (language = 'en-US'));
+  renderFilmCards(data);
+}
+
+async function renderFilmCards(data) {
+  const genresData = (await getAllGenres('en-US')).genres;
+  const filmList = data.results.map(
+    ({ title, vote_average, genre_ids, release_date, poster_path }) => {
+      const genresList = genre_ids
+        .map(idNum => {
+          for (const obj of genresData) {
+            if (idNum === obj.id) {
+              return (id = obj.name);
+            }
+          }
+        })
+        .join(', ');
+      return `<li class="card__item">
+    <a class="card__link" href = "">
+        <img class="card__img"
+        src="https://image.tmdb.org/t/p/original/${poster_path}"
+        alt=""
+    />
+    </a>
+    <h2 class="card__title">${title}</h2>
+    <div class="card__text">
+        <p class="card__info">${genresList} | ${release_date
+        .split('', 4)
+        .join('')}</p>
+        <div class="card__rating">${vote_average}</div>
+    </div>
+</li>`;
+    }
+  );
+  galleryListEl.insertAdjacentHTML('beforeend', filmList.join(''));
+}
+request();
