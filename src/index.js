2ad832ecb712cc848efea7d4643040d8ecf4c4d7
VladaBoiko <bermezha@gmail.com>
2022-09-28 18:28:00 +0300

    push

--- src/index.js
+++ src/index.js
@@ -25,81 +25,18 @@ dataMovieList().then(data => {
   );
   galleryListEl.insertAdjacentHTML('beforeend', filmList.join(''));
 });
+const allGenres = [];
 async function genreDecoder(genre_ids) {
-  const genresData = await dataGenre((language = 'en-US'));
-  const allGenres = [];
-  const data = genresData.genres;
-  data.map(genre => {
-    if (genre_ids.includes(genre.id)) {
-      allGenres.push(genre.name);
-
-function genreDecoder(array) {
-  const arrayOfgenres = array.map(a => {
-    switch (a) {
-      case 28:
-        return `Action`;
-        break;
-      case 12:
-        return `Adventure`;
-        break;
-      case 16:
-        return `Animation`;
-        break;
-      case 35:
-        return `Comedy`;
-        break;
-      case 80:
-        return `Crime`;
-        break;
-      case 99:
-        return `Documentary`;
-        break;
-      case 18:
-        return `Drama`;
-        break;
-      case 10751:
-        return `Family`;
-        break;
-      case 14:
-        return `Fantasy`;
-        break;
-      case 36:
-        return `History`;
-        break;
-      case 27:
-        return `Horror`;
-        break;
-      case 10402:
-        return `Music`;
-        break;
-      case 9648:
-        return `Mystery`;
-        break;
-      case 10749:
-        return `Romance`;
-        break;
-      case 37:
-        return `Western`;
-        break;
-      case 878:
-        return `Science Fiction`;
-        break;
-      case 10770:
-        return `TV Movie`;
-        break;
-      case 53:
-        return `Thriller`;
-        break;
-      case 10752:
-        return `War`;
-        break;
-      default:
-        return 'new genre';
-        break;
-    }
+  const genresData = await dataGenre((language = 'en-US')).then(data => {
+    const allDataGenres = data.genres;
+    allDataGenres.map(genre => {
+      if (genre_ids.includes(genre.id)) {
+        allGenres.push(genre.name);
+      }
+    });
+    console.log(allGenres.join(', '));
+    return allGenres.join(', ');
   });
-  console.log(allGenres.join(', '));
-  return allGenres.join(', ');
   // const arrayOfgenres = array.map(a => {
   //   switch (a) {
   //     case 28:
