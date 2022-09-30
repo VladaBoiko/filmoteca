767b1966d1a521cada1fc25105c8474ba9cc9bd0
KautTanya <kaut.tanya.s@gmail.com>
2022-10-01 02:44:45 +0300

    style

--- src/js/changeTheme.js
+++ src/js/changeTheme.js
@@ -1,10 +1,12 @@
-const changeMarker = document.querySelector('.theme-switch__marker')
+const changeMarker = document.querySelector('#theme-marker')
 const changeBox = document.querySelector('#theme-switch-toggle');
 changeBox.addEventListener('change', ()=>{
     if (changeBox.checked) {
+      
         darkTheme();
             	}
         	else {
+            
                 lightTheme()
 
         	}
@@ -12,13 +14,16 @@ changeBox.addEventListener('change', ()=>{
  function darkTheme(){
     document.body.classList.add('dark-theme');
     localStorage.setItem('theme', 'dark-theme');
-    changeMarker.translateX(0);
+   
+    
+    
  }
 
  function lightTheme(){
     document.body.classList.remove('dark-theme');
     localStorage.removeItem('theme', 'dark-theme');
-    // changeMarker.translateX(0);
+    
+ 
  }
 
  if (localStorage.getItem('theme') === 'dark-theme') {
