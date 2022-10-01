a18d3c1e8a07c80afdd71b4eaa6d009b047164ad
Vadim luginets <pebeeo@gmail.com>
2022-10-01 16:28:12 +0300

    Пагінація для пошуку
    Пагінація по пошуку робочий код

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,3 +1,4 @@
+import SweetScroll from 'sweet-scroll';
 export const refs = {
   headerIndex: document.querySelector('.header-index'),
   headerLibrary: document.querySelector('.header__library'),
@@ -14,6 +15,23 @@ export const refs = {
   // interface
   // genere
   // movie
+  headerEl: document.querySelector('header'),
+  pageNavDivEl: document.querySelector('.pagination'),
+  prevBtnEl: document.querySelector('.pagination_previousBtn'),
+  nextBtnEl: document.querySelector('.pagination_nextBtn'),
+  currentPageEl: document.querySelector('.pagination_currentPage'),
+  firstPageEl: document.querySelector('.pagination_firstPage'),
+  lastPageEl: document.querySelector('.pagination_lastPage'),
+  leftPointsEl: document.querySelector('.pagination_leftPoints'),
+  rightPointsEl: document.querySelector('.pagination_rightPoints'),
+  rightFirstPageEl: document.querySelector('.pagination_rightFirstPage'),
+  rightSecondPageEl: document.querySelector('.pagination_rightSecondPage'),
+  leftFirstPageEl: document.querySelector('.pagination_leftFirstPage'),
+  leftSecondPageEl: document.querySelector('.pagination_leftSecondPage'),
+  currentPage: 1,
+  totalPages: 1,
+  scroller: new SweetScroll({ duration: 6000 }),
+  pagSerchQuery: '',
   // footer
   // modalMovie
   openModalMovie: document.querySelector('[data-movie-modal-open]'),
