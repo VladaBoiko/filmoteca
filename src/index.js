40ce18cf906fd7f3bc53e7e09bd14f6ce284f202
VladimirSt13 <Vladimir.Stognij@gmail.com>
2022-10-02 18:43:03 +0300

    fix-loading

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
+import './sass/index.scss';
 import './js/headerToggle';
 import './js/render';
 import './js/modalMovie';
-import './js/render';
 import './js/changeTheme';
 import './js/search';
 import './js/auth/auth';
