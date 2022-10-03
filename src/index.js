977719b33a920fb5d051a86f43b9e3739ded06da
lavrinenkokyiv <88554016+lavrinenkokyiv@users.noreply.github.com>
2022-10-03 22:43:27 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './sass/index.scss';
 import './js/headerToggle';
 import './js/render';
 import './js/modalMovie';
+import './js/modalTeam';
 import './js/changeTheme';
 import './js/search';
 import './js/goTopBtn';
