56c8a95b2d8353d15ba553496afd173de39003e5
lavrinenkokyiv <88554016+lavrinenkokyiv@users.noreply.github.com>
2022-10-04 03:19:59 +0300

    fix.skroll

--- src/js/modalTeam.js
+++ src/js/modalTeam.js
@@ -5,19 +5,23 @@ const isVisible = "is-visible";
 for (const el of openEls) {
   el.addEventListener("click", function() {
     const modalId = this.dataset.open;
+    document.body.style.overflow = 'hidden';
     document.getElementById(modalId).classList.add(isVisible);
+    
   });
 }
 
 for (const el of closeEls) {
   el.addEventListener("click", function() {
     this.parentElement.parentElement.parentElement.classList.remove(isVisible);
+    document.body.style.overflow = 'visible';
   });
 }
 
 document.addEventListener("click", e => {
   if (e.target == document.querySelector(".modal.is-visible")) {
     document.querySelector(".modal.is-visible").classList.remove(isVisible);
+    document.body.style.overflow = 'visible';
   }
 });
 
@@ -25,5 +29,7 @@ document.addEventListener("keyup", e => {
   // if we press the ESC
   if (e.key == "Escape" && document.querySelector(".modal.is-visible")) {
     document.querySelector(".modal.is-visible").classList.remove(isVisible);
+    document.body.style.overflow = 'visible';
   }
 });
+
